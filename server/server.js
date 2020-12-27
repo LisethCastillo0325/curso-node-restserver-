931ec631b04653a32483e804d3bb1274838c8ee3
Liseth Castillo <lisethcastillo.0325@gmail.com>
2020-12-27 16:10:28 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require('../config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+ 
+app.use( require('./routes/usuario') );
+
+mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Base de datos ONLINE');
+});
+
+ 
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto: ', process.env.PORT);
+});
